0ebede23245009c73e2648093a618cd2ca6323b7
unknown <nik.abr96@gmail.com>
2020-11-07 19:06:23 +0200

    learning vue

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
+      nulla temporibus, quam sint iste exercitationem tenetur ducimus
+      dignissimos, veritatis ex labore? Facilis ratione dolorem corporis
+      assumenda quasi porro numquam voluptatibus?
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
